aaf33c9a4f0cfc16495a880388b7d63f6f380d52
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2016-04-07 10:20:00 +0300

    1- init game view.

--- Score/ScoreViewController.m
+++ Score/ScoreViewController.m
@@ -10,7 +10,7 @@
 #import "MZTimerLabel.h"
 #import "JDAvatarProgress.h"
 
-@interface ScoreViewController ()
+@interface ScoreViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @end
 
@@ -26,11 +26,85 @@
     __weak IBOutlet UITableView *tableView;
     NSMutableArray* scoresDataSource;
     NSDictionary* gameDictionary;
+    NSString* userName;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    
+    [self initData];
+    [self initTableView];
+}
+
+
+#pragma mark views methods
+-(void)initData
+{
+    userName = [[NSUserDefaults standardUserDefaults] objectForKey:@"username"];
+    scoresDataSource = [[NSMutableArray alloc]init];
+    gameDictionary = [[NSDictionary alloc]init];
+    
+    
+    dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSData* data = [[NSData alloc]initWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:@"getScores.php?name=%@",userName]]];
+        scoresDataSource = [[NSMutableArray alloc]initWithArray:[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil]];
+        dispatch_async( dispatch_get_main_queue(), ^{
+            [self initTableView];
+        });
+    });
+    
+    
+    dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSData* data = [[NSData alloc]initWithContentsOfURL:[NSURL URLWithString:@"getGame.php"]];
+        gameDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+        dispatch_async( dispatch_get_main_queue(), ^{
+            [self initGameView];
+        });
+    });
+    
+}
+
+
+-(void)initGameView
+{
+    [firstTeamIcon setImageWithURL:[NSURL URLWithString:[gameDictionary objectForKey:@"firstTeamIcon"]]
+                            placeholder:[UIImage imageNamed:@"female-placeholder.jpg"]
+                          progressColor:[UIColor orangeColor]
+                    progressBarLineWidh:JDAvatarDefaultProgressBarLineWidth
+                            borderWidth:JDAvatarDefaultBorderWidth
+                            borderColor:nil
+                             completion:^(UIImage * resultImage, NSError * error){}];
+    
+    [secondItemIcon setImageWithURL:[NSURL URLWithString:[gameDictionary objectForKey:@"secondTeamIcon"]]
+                       placeholder:[UIImage imageNamed:@"female-placeholder.jpg"]
+                     progressColor:[UIColor orangeColor]
+               progressBarLineWidh:JDAvatarDefaultProgressBarLineWidth
+                       borderWidth:JDAvatarDefaultBorderWidth
+                       borderColor:nil
+                        completion:^(UIImage * resultImage, NSError * error){}];
+    
+    [firstTeamName setText:[gameDictionary objectForKey:@"firstTeamName"]];
+    [secondTeamName setText:[gameDictionary objectForKey:@"secondTeamName"]];
+    
+    NSDateFormatter* formatter = [[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"dd-MM-YYYY HH:mm"];
+    
+    NSDate* currentDate = [NSDate date];
+    NSDate* gameDate = [formatter dateFromString:[gameDictionary objectForKey:@"matchDate"]];
+    
+    counterLabel.timeLabel.font = [UIFont systemFontOfSize:20.0f];
+    counterLabel.timeLabel.textColor = [UIColor redColor];
+    [counterLabel setCountDownTime:[gameDate timeIntervalSinceDate:currentDate]];
+}
+
+-(void)initTableView
+{
+    [tableView setDelegate:self];
+    [tableView setDataSource:self];
+    
+    [tableView reloadData];
+    [tableView setNeedsDisplay];
 }
 
 - (void)didReceiveMemoryWarning {
